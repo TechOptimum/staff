53c182cde28c33710478161c07ffb7fb3712dd68
Aditya <adityasahas2025@gmail.com>
2023-06-30 21:02:18 -0700

    commit 1 (media)

--- pages/api/sendVolunteerInfo.ts
+++ pages/api/sendVolunteerInfo.ts
@@ -1,63 +1,67 @@
+import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
-import { NextApiRequest, NextApiResponse } from "next";
+import FormData from 'form-data';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === "POST") {
-    const { name, discord, department, task, hours, otherOrg } = req.body;
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const { file, name, discord, department, task, hours, otherOrg } = req.body;
+
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', 'c1isclgp');
+
+    const isPDF = file.type === "application/pdf";
+    const url = isPDF 
+      ? 'https://api.cloudinary.com/v1_1/dfvsg4dek/raw/upload' 
+      : 'https://api.cloudinary.com/v1_1/dfvsg4dek/image/upload';
 
     try {
-      // Discord webhook URL
-      const url =
-        "https://discord.com/api/webhooks/1124212516965326848/a8vQjZRBDofAEGAffqysVRrBdvJB4cTBPfA4p_DQxIpci_OwcOqFlxqG-ZafubEZBJqY";
-
-      const result = await axios.post(url, {
-        embeds: [
-          {
-            title: `Volunteer hours logged by ${name}`,
-            fields: [
-              {
-                name: "Discord",
-                value: discord,
-                inline: true,
-              },
-              {
-                name: "Department",
-                value: department,
-                inline: true,
-              },
-              {
-                name: "Task",
-                value: task,
-                inline: false,
-              },
-              {
-                name: "Hours",
-                value: hours.toString(),
-                inline: true,
-              },
-              {
-                name: "Any hours from other organization?",
-                value: otherOrg ? "Yes" : "No",
-                inline: true,
-              },
-            ],
-          },
-        ],
+      const response = await axios.post(url, formData, {
+        headers: formData.getHeaders()
       });
 
-      if (result.status === 204) {
-        res.status(200).json({ success: true });
-      } else {
-        res.status(500).json({ success: false });
-      }
-    } catch (err) {
-      res.status(500).json({ success: false });
+      const fileUrl = response.data.secure_url;
+
+      const webhookUrl = 'https://discord.com/api/webhooks/1124212516965326848/a8vQjZRBDofAEGAffqysVRrBdvJB4cTBPfA4p_DQxIpci_OwcOqFlxqG-ZafubEZBJqY';
+      await axios.post(webhookUrl, {
+        embeds: [{
+          title: `Volunteer hours logged by ${name}`,
+          fields: [
+            {
+              name: 'Discord',
+              value: discord,
+              inline: true,
+            },
+            {
+              name: 'Department',
+              value: department,
+              inline: true,
+            },
+            {
+              name: 'Task',
+              value: task,
+              inline: false,
+            },
+            {
+              name: 'Hours',
+              value: hours.toString(),
+              inline: true,
+            },
+            {
+              name: 'Other organization?',
+              value: otherOrg ? 'Yes' : 'No',
+              inline: true,
+            },
+          ],
+          image: { url: fileUrl },
+        }],
+      });
+
+      res.status(200).json({ message: 'Volunteer info and file sent successfully.' });
+    } catch (error) {
+      res.status(500).json({ error: 'Upload failed.' });
     }
   } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end("Method Not Allowed");
+    res.status(405).json({ error: 'Invalid request method.' });
   }
 }
--- pages/volunteer-hours.tsx
+++ pages/volunteer-hours.tsx
@@ -1,9 +1,9 @@
 import type { NextPage } from "next";
 import { useState } from "react";
-import axios from "axios";
 import { Nav } from "../components/navbar/navbar";
 import { Layout } from "../components/navbar/layout";
 import { Box } from "../components/styles/box";
+import axios from "axios";
 import {
   Button,
   Input,
@@ -15,6 +15,7 @@ import {
 } from "@nextui-org/react";
 
 const Volunteer: NextPage = () => {
+  const [file, setFile] = useState<File | null>(null);
   const [name, setName] = useState("");
   const [discord, setDiscord] = useState("");
   const [department, setDepartment] = useState("");
@@ -22,24 +23,43 @@ const Volunteer: NextPage = () => {
   const [hours, setHours] = useState(0);
   const [otherOrg, setOtherOrg] = useState(false);
 
+  const handleFileUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setFile(e.target.files[0]);
+    }
+  };
+
   const handleSubmit = async () => {
+    // Call handleFileUpload when the form is submitted
+    const fileInput = document.querySelector("#file-upload");
+    if (fileInput) {
+      handleFileUpload({
+        target: fileInput,
+      } as React.ChangeEvent<HTMLInputElement>);
+    }
+  };
+  const handleFormSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    if (file) {
+      formData.append("file", file);
+    }
+    formData.append("name", name);
+    formData.append("discord", discord);
+    formData.append("department", department);
+    formData.append("task", task);
+    formData.append("hours", hours.toString());
+    formData.append("otherOrg", otherOrg.toString());
+
     try {
-      const res = await axios.post('/api/sendVolunteerInfo', {
-        name,
-        discord,
-        department,
-        task,
-        hours,
-        otherOrg
+      await axios.post("/api/sendVolunteerInfo", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
       });
 
-      if(res.data.success) {
-        alert('Hours submitted successfully!');
-      } else {
-        alert('An error occurred, please try again.');
-      }
-    } catch(err) {
-      console.error(err);
+      // Clear form fields or handle response as needed
+    } catch (error) {
+      console.error("Error uploading file: ", error);
     }
   };
 
@@ -47,6 +67,8 @@ const Volunteer: NextPage = () => {
     <Layout>
       <Divider />
       <Box
+        as="form"
+        onSubmit={handleFormSubmit}
         style={{
           display: "flex",
           flexDirection: "column",
@@ -57,36 +79,32 @@ const Volunteer: NextPage = () => {
           paddingLeft: "9em",
           boxSizing: "border-box",
         }}
-        as="main"
       >
         <Text css={{ mb: "2rem" }} h2>
           Volunteer Hours
         </Text>
-        <Input 
-          css={{ mb: "2rem" }} 
-          color="primary" 
-          clearable 
-          bordered 
-          label="Name" 
-          required 
-          value={name} 
+        <Input
+          css={{ mb: "2rem" }}
+          color="primary"
+          clearable
+          bordered
+          label="Name"
+          required
           onChange={(e) => setName(e.target.value)}
         />
-        <Input 
-          css={{ mb: "2rem" }} 
-          color="primary" 
-          clearable 
-          bordered 
-          label="Discord" 
-          required 
-          value={discord} 
+        <Input
+          css={{ mb: "2rem" }}
+          status="primary"
+          clearable
+          bordered
+          label="Discord"
+          required
           onChange={(e) => setDiscord(e.target.value)}
         />
-        <Radio.Group 
-          css={{ mb: "2rem" }} 
+        <Radio.Group
+          css={{ mb: "2rem" }}
           label="Departments"
-          value={department}
-          onChange={(val) => setDepartment(val)}
+          onChange={(val: any) => setDepartment(val.target)}
         >
           <Radio value="Web Development Team">Web Development Team</Radio>
           <Radio value="Education Team">Education Team</Radio>
@@ -95,34 +113,39 @@ const Volunteer: NextPage = () => {
           <Radio value="Marketing/Design Team">Marketing/Design Team</Radio>
           <Radio value="Other">Other</Radio>
         </Radio.Group>
-        <Textarea 
-          css={{ mb: "2rem" }} 
-          color="primary" 
-          bordered 
-          label="What task did you do?" 
-          required 
-          value={task} 
+        <Textarea
+          css={{ mb: "2rem" }}
+          status="primary"
+          bordered
+          label="What task did you do?"
+          required
           onChange={(e) => setTask(e.target.value)}
         />
-        <Input 
-          css={{ mb: "2rem" }} 
-          color="primary" 
-          clearable 
-          bordered 
-          type="number" 
-          label="Hours for the task?" 
-          required 
-          value={hours} 
+        <Input
+          css={{ mb: "2rem" }}
+          status="primary"
+          clearable
+          bordered
+          type="number"
+          label="Hours for the task?"
+          required
           onChange={(e) => setHours(Number(e.target.value))}
         />
-        <Checkbox 
-          label="Do you have any hours from another organization?" 
-          onChange={(checked) => setOtherOrg(checked)}
+        <Checkbox
+          label="Do you have any hours from another organization?"
+          onChange={setOtherOrg}
+        />
+        <input
+          id="file-upload"
+          type="file"
+          accept=".jpg,.jpeg,.png,.pdf"
+          onChange={handleFileUpload} // This is where the change happens
         />
         <Text css={{ mb: "2rem" }} h4>
-          Send proof of volunteering in a Discord Ticket if necessary
+          Upload proof of your hours from another organization here (optional){" "}
+          <i>.jpg,.jpeg,.png,.pdf</i>
         </Text>
-        <Button color="primary" onClick={handleSubmit}>Submit</Button>
+        <Button type="submit">Submit</Button>
       </Box>
     </Layout>
   );
