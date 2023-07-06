a2773108130c14142056fcc23a6924c422653f40
PranTanTheMan <46548127+PranTanTheMan@users.noreply.github.com>
2023-07-06 18:37:55 -0500

    Maybe this works?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,41 +1,41 @@
-import '../styles/globals.css';
-import type {AppProps} from 'next/app';
-import {createTheme, NextUIProvider} from '@nextui-org/react';
-import {ThemeProvider as NextThemesProvider} from 'next-themes';
-import {Nav} from '../components/navbar/navbar';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { createTheme, NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { Nav } from "../components/navbar/navbar";
 
 const lightTheme = createTheme({
-   type: 'light',
-   theme: {
-      colors: {
-         h1: "#000000",
-      },
-   },
+  type: "light",
+  theme: {
+    colors: {
+      h1: "#000000",
+    },
+  },
 });
 
 const darkTheme = createTheme({
-   type: 'dark',
-   theme: {
-      colors: {},
-   },
+  type: "dark",
+  theme: {
+    colors: {},
+  },
 });
 
-function MyApp({Component, pageProps}: AppProps) {
-   return (
-      <NextThemesProvider
-         defaultTheme="dark"
-         attribute="class"
-         value={{
-            light: lightTheme.className,
-            dark: darkTheme.className,
-         }}
-      >
-         <NextUIProvider>
-            <Nav />
-            <Component {...pageProps} />
-         </NextUIProvider>
-      </NextThemesProvider>
-   );
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <NextThemesProvider
+      defaultTheme="dark"
+      attribute="class"
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className,
+      }}
+    >
+      <NextUIProvider>
+        <Nav />
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </NextThemesProvider>
+  );
 }
 
 export default MyApp;
