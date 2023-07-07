ad521cec572b76afa4faa2d00afcea1591fa0d5f
PranTanTheMan <46548127+PranTanTheMan@users.noreply.github.com>
2023-07-06 19:06:24 -0500

    Maybe this works?

--- pages/department/web-dev.jsx
+++ pages/department/web-dev.jsx
@@ -6,24 +6,24 @@ import { NotionRenderer } from "react-notion";
 export async function getStaticProps() {
   const data = await fetch(
     "https://notion-api.splitbee.io/v1/page/c005396a64804424945e245223b2ae4a"
-  ).then(res => res.json());
+  ).then((res) => res.json());
 
   return {
     props: {
-      blockMap: data
-    }
+      blockMap: data,
+    },
   };
 }
 
 const Handbook = ({ blockMap }) => (
   <div style={{ maxWidth: 768, color: "white !important", margin: "auto" }}>
     <Text>
-        <h2>Web Development Volunteer Handbook</h2>
+      <h2>Web Development Volunteer Handbook</h2>
     </Text>
     <NotionRenderer blockMap={blockMap} />
   </div>
 );
 
-Handbook.displayName = 'Handbook';
+Handbook.displayName = "Handbook";
 
 export default Handbook;
